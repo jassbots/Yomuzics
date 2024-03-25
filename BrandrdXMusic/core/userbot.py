f2dc5a083681a8b71e330a1486e38111dd2f64f3
jassbots <157136102+jassbots@users.noreply.github.com>
2024-03-25 22:33:47 +0530

    Update userbot.py

--- BrandrdXMusic/core/userbot.py
+++ BrandrdXMusic/core/userbot.py
@@ -51,9 +51,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDED_PAID_CC")
-                await self.one.join_chat("BRANDRD_BOT")
+                await self.one.join_chat("punjabiii_chat")
+                await self.one.join_chat("unknown_bande")
+                await self.one.join_chat("entity_unavailable")
             except:
                 pass
             assistants.append(1)
@@ -73,9 +73,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDED_PAID_CC")
-                await self.one.join_chat("BRANDRD_BOT")
+                await self.two.join_chat("punjabiii_chat")
+                await self.one.join_chat("unknown_bande")
+                await self.one.join_chat("entity_unavailable")
             except:
                 pass
             assistants.append(2)
@@ -95,9 +95,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDED_PAID_CC")
-                await self.one.join_chat("BRANDRD_BOT")
+                await self.three.join_chat("Punjabiii_chat")
+                await self.one.join_chat("unknown_bande")
+                await self.one.join_chat("entity_unavailable")
             except:
                 pass
             assistants.append(3)
@@ -117,9 +117,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDED_PAID_CC")
-                await self.one.join_chat("BRANDRD_BOT")
+                await self.four.join_chat("Punjabiii_chat")
+                await self.one.join_chat("unknown_bande")
+                await self.one.join_chat("entity_unavailable")
             except:
                 pass
             assistants.append(4)
@@ -139,9 +139,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("BRANDED_WORLD")
-                await self.one.join_chat("BRANDED_PAID_CC")
-                await self.one.join_chat("BRANDRD_BOT")
+                await self.five.join_chat("Punjabiii_chat")
+                await self.one.join_chat("unknown_bande")
+                await self.one.join_chat("entity_unavailable")
             except:
                 pass
             assistants.append(5)
